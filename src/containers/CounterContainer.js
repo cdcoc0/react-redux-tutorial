2c50eff6f301223ac041284c809d9cae78079bca
velykitty <jiyunee13@gmail.com>
2021-05-10 18:13:11 +0900

    modify to make container easier

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import Counter from '../components/Counter'
 import {increase, decrease} from '../modules/counter';
@@ -9,20 +10,32 @@ const CounterContainer = ({number, increase, decrease}) => {
     );
 };
 
-const mapStateToProps = state => ({
-    number: state.counter.number
-});
+// const mapStateToProps = state => ({
+//     number: state.counter.number
+// });
 
-const mapDispatchToProps = dispatch => ({
-    increase: () => {
-        dispatch(increase());
-    },
-    decrease: () => {
-        dispatch(decrease());
-    }
-});
+// const mapDispatchToProps = dispatch => ({
+//     increase: () => {
+//         dispatch(increase());
+//     },
+//     decrease: () => {
+//         dispatch(decrease());
+//     }
+// });
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps,
+    state => ({
+        number: state.counter.number
+    }),
+    // 1) dispatch => ({
+    //     increase: () => dispatch(increase()),
+    //     decrease: () => dispatch(decrease())
+    // })
+
+    // 2) dispatch => 
+    //     bindActionCreators(
+    //         {increase, decrease},
+    //         dispatch
+    //     )
+    {increase, decrease} //두 번째 파라미터를 아예 객체 형태로 넣어 주면 connect 함수가 내부적으로 bindActionCreators 작업을 해줌
 )(CounterContainer);
