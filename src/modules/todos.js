c45c3435d8151c779e6e7e1288d483f1b6fbe686
velykitty <jiyunee13@gmail.com>
2021-05-12 07:30:39 +0900

    immer 적용

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,4 +1,5 @@
 import {createAction, handleActions} from 'redux-actions';
+import produce from 'immer';
 
 const CHANGE_INPUT = 'todos/CHANGE_INPUT'; //인풋 값 변경
 const INSERT = 'todos/INSERT'; // 새로운 todo 등록
@@ -55,23 +56,37 @@ const initialState = {
 const todos = handleActions (
     {
         //[CHANGE_INPUT]: (state, action) => ({...state, input: action.payload}),
-        [CHANGE_INPUT]: (state, {payload: input}) => ({...state, input}),
+        [CHANGE_INPUT]: (state, {payload: input}) => //({...state, input}),
+            produce(state, draft => {
+                draft.input = input;
+            }),
     
         //[INSERT]: (state, action) => ({...state, todos: state.todos.concat(action.payload)}),
-        [INSERT]: (state, {payload: todo}) => ({...state, todos: state.todos.concat(todo)}),
+        [INSERT]: (state, {payload: todo}) => //({...state, todos: state.todos.concat(todo)}),
+            produce(state, draft => {
+                draft.todos.push(todo);
+            }),
     
     
         // [TOGGLE]: (state, action) => ({...state, 
         //     todos: state.todos.map(
         //         todo => todo.id === action.payload ? {...todo, done: !todo.done} : todo)}),
-        [TOGGLE]: (state, {payload: id}) => ({...state, 
-            todos: state.todos.map(
-                todo => todo.id === id ? {...todo, done: !todo.done} : todo)}),
+        [TOGGLE]: (state, {payload: id}) => //({...state, 
+            // todos: state.todos.map(
+            //     todo => todo.id === id ? {...todo, done: !todo.done} : todo)}),
+            produce(state, draft => {
+                const todo = draft.todos.find(todo => todo.id ===id);
+                todo.done = !todo.done;
+            }),
 
         // [REMOVE]: (state, action) => ({...state, 
         //     todos: state.todos.filter(todo => todo.id !== action.payload)})
-        [REMOVE]: (state, {payload: id}) => ({...state, 
-            todos: state.todos.filter(todo => todo.id !== id)})
+        [REMOVE]: (state, {payload: id}) => //({...state, 
+            //todos: state.todos.filter(todo => todo.id !== id)})
+            produce(state, draft => {
+                const index = draft.todos.findIndex(todo => todo.id === id);
+                draft.todos.splice(index, 1);
+            })
     },
     initialState
 );
