5a67707d3419b7cffd8a5526f85b0f79f2e47719
velykitty <jiyunee13@gmail.com>
2021-05-12 12:15:42 +0900

    Hooks를 사용해 Container Component 만들기 -CounterContainter

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {bindActionCreators} from 'redux';
-import {connect} from 'react-redux';
+import {connect, useSelector, useDispatch} from 'react-redux';
 import Counter from '../components/Counter'
 import {increase, decrease} from '../modules/counter';
 
-const CounterContainer = ({number, increase, decrease}) => {
+const CounterContainer = () => { //{number, increase, decrease}
+    const number = useSelector(state => state.counter.number);
+    const dispatch = useDispatch();
     return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    <Counter number={number} onIncrease={() => dispatch(increase())} onDecrease={() => dispatch(decrease())} />
     );
 };
 
@@ -23,19 +25,21 @@ const CounterContainer = ({number, increase, decrease}) => {
 //     }
 // });
 
-export default connect(
-    state => ({
-        number: state.counter.number
-    }),
-    // 1) dispatch => ({
-    //     increase: () => dispatch(increase()),
-    //     decrease: () => dispatch(decrease())
-    // })
+export default CounterContainer;
 
-    // 2) dispatch => 
-    //     bindActionCreators(
-    //         {increase, decrease},
-    //         dispatch
-    //     )
-    {increase, decrease} //두 번째 파라미터를 아예 객체 형태로 넣어 주면 connect 함수가 내부적으로 bindActionCreators 작업을 해줌
-)(CounterContainer);
+// connect(
+//     state => ({
+//         number: state.counter.number
+//     }),
+//     // 1) dispatch => ({
+//     //     increase: () => dispatch(increase()),
+//     //     decrease: () => dispatch(decrease())
+//     // })
+
+//     // 2) dispatch => 
+//     //     bindActionCreators(
+//     //         {increase, decrease},
+//     //         dispatch
+//     //     )
+//     {increase, decrease} //두 번째 파라미터를 아예 객체 형태로 넣어 주면 connect 함수가 내부적으로 bindActionCreators 작업을 해줌
+// )(CounterContainer);
